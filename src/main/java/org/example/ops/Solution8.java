4112f914c3691214dbe06c5e288f997cf9ebf322
autunder <2206978559@qq.com>
2024-10-20 11:05:06 +0800

    add solution

---
+++ src/main/java/org/example/ops/Solution8.java
@@ -0,0 +1,79 @@
+package org.example.ops;
+
+/**
+ * @description:
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ *
+ *
+ * 提示：
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ *
+ */
+public class Solution8 {
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+        //修改1，越界边界条件错误，c>0改为c<0
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+        //修改2，此处应是将访问过的地方标记，为防止重复访问，应标记为'0'而不是'1'
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        //修改3，当只有一个单位时，应依旧考虑是否为岛屿，将<=1改为==0
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;//行数
+        int nc = grid[0].length;//列数
+        int num_islands = 0;//岛屿计数
+        for (int r = 0; r < nr; ++r) {
+            //修改4，循环迭代变量应为c,r<nc改为c<nc
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+}
